cf45a9ef5ca562c9b84b18942fed94a3ffea8d76
delucis <swithinbank@gmail.com>
2017-07-27 18:00:27 +0200

    Create module scaffolding.

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * @module reading-data
+ * @requires winston
+ */
+
+const log = require('winston')
+
+const ReadingData = (function () {
+  // PRIVATE VARIABLES
+  return {
+    // PUBLIC VARIABLES
+  }
+}())
+
+module.exports = ReadingData
